1726d2ee23406212adf7f94942810d3583ee8b06
Rafael Morch <rafael.morch@gmail.com>
2026-02-11 21:43:25 -0500

    fix(iOS): enable safe area viewport-fit cover

--- app/layout.tsx
+++ app/layout.tsx
@@ -33,7 +33,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
       <head>
         <meta
           name="viewport"
-          content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no"
+          content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no, viewport-fit=cover"
         />
       </head>
       <body className={rowdies.className}>
@@ -44,4 +44,3 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
     </html>
   );
 }
-
